14304ce256126e9878fe5f45ab1787f36ca9b7dc
Matthew Epler <matthewepler@gmail.com>
2016-09-20 10:49:22 -0400

    updates structure, webpack config, and core

---
+++ app/components/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+// styles
+import '../styles/base.scss';
+
+
+class App extends React.Component {
+    constructor() {
+        super();
+        this.state = {};
+    }
+
+
+    render() {
+        return( <h1> Hello React </h1> )
+    }
+}
+
+export default App;
--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,16 @@
+const Dotenv = require('dotenv-webpack');
+
 module.exports = {
 	context: __dirname,
-	entry: './main.js',
+	entry: './app/main.js',
 	output: {
-		path: './',
+		path: __dirname + '/build',
 		filename: 'bundle.js',
+		publicPath: 'build',
 	},
 	devServer: {
 		inline: true,  // hot reloading without webpack status bar
-		port: 1111,    // channge to whatever
+		port: 8888,    // channge to whatever
 	},
 	devtool: 'source-map',
 	module: {
@@ -26,10 +29,32 @@ module.exports = {
 				test: /\.scss$/, // for my scss sheets. imported in App.js
 				exclude: /node_modules/,
 				loaders: ["style", "css", "sass"]
+			},
+			{ 
+				test: /\.(png|woff|woff2|eot|ttf|svg)$/, 
+				loader: 'url-loader?limit=100000' 
+			},
+			{ 
+				test: /\.json$/, 
+				loader: 'json-loader' 
 			}
 		]
 	},
+	node: {
+	    console: true,
+	    fs: 'empty',
+	    net: 'empty',
+	    tls: 'empty'
+  	},
 	postcss: function () {
         return [precss, autoprefixer];
-    }
+    },
+    plugins: [
+	    new Dotenv({
+	      path: './.env', // can be ommitted as this is the default 
+	      safe: false, // make true to use dotenv-safe and require variables 
+	      sample: './.env.example', // if safe=true, use this to define the safe env 
+	      systemvars: false // if true, also loads system env variables 
+	    })
+  	]
 }
